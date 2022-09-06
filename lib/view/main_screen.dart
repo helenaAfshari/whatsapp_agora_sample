dbd8e1e91b3891789743e30bc68eaf9be876593f
sasansafari <sasansafari19@gmail.com>
2022-09-06 06:35:56 -0700

    status screen scafold updated

--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -53,6 +53,7 @@ class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
 
     return SafeArea(
         child: Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
         actions: [
@@ -69,99 +70,167 @@ class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
         ],
         title: const Text("WhatsApp"),
       ),
-      body: Column(children: [
-        Container(
-          height: 35,
-          color: whatsappColor,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: Get.width / 13,
-                child: const Icon(
-                  Icons.camera,
-                  color: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Container(
+            height: 35,
+            color: whatsappColor,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                //camnera
+                SizedBox(
+                  width: Get.width / 13,
+                  child: const Icon(
+                    Icons.camera,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              Expanded(
-                child: SizedBox(
-                  height: 34,
-                  child: Center(
-                      child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: tabTitleList.length,
-                          shrinkWrap: true,
-                          itemBuilder: ((context, index) => GestureDetector(
-                                onTap: (() {
-                                  selectedTabIndex.value = index;
-                                }),
-                                child: Obx(
-                                  () => SizedBox(
-                                      width: tabSize,
-                                      child: Center(
-                                          child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceAround,
-                                        children: [
-                                          Text(
-                                            tabTitleList[index],
-                                            style:
-                                                index == selectedTabIndex.value
-                                                    ? selectedTabBarTextStyle
-                                                    : unselectedTabBarTextStyle,
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.symmetric(
-                                                horizontal: 4),
-                                            child: AnimatedOpacity(
-                                                duration: const Duration(
-                                                    milliseconds: 500),
-                                                opacity: index ==
-                                                        selectedTabIndex.value
-                                                    ? 1
-                                                    : 0,
-                                                child: Container(
-                                                  height: 2,
-                                                  color: Colors.white,
-                                                )),
-                                          )
-                                        ],
-                                      ))),
-                                ),
-                              )))),
+                //tabs
+                Expanded(
+                  child: SizedBox(
+                    height: 34,
+                    child: Center(
+                        child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: tabTitleList.length,
+                            shrinkWrap: true,
+                            itemBuilder: ((context, index) => GestureDetector(
+                                  onTap: (() {
+                                    selectedTabIndex.value = index;
+                                  }),
+                                  child: Obx(
+                                    () => SizedBox(
+                                        width: tabSize,
+                                        child: Center(
+                                            child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceAround,
+                                          children: [
+                                            Text(
+                                              tabTitleList[index],
+                                              style: index ==
+                                                      selectedTabIndex.value
+                                                  ? selectedTabBarTextStyle
+                                                  : unselectedTabBarTextStyle,
+                                            ),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 4),
+                                              child: AnimatedOpacity(
+                                                  duration: const Duration(
+                                                      milliseconds: 500),
+                                                  opacity: index ==
+                                                          selectedTabIndex.value
+                                                      ? 1
+                                                      : 0,
+                                                  child: Container(
+                                                    height: 2,
+                                                    color: Colors.white,
+                                                  )),
+                                            )
+                                          ],
+                                        ))),
+                                  ),
+                                )))),
+                  ),
+                )
+              ],
+            ),
+          ),
+
+          //chats
+          Obx(
+            () => IndexedStack(
+              index: selectedTabIndex.value,
+              children: [
+                chatScreen(),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //Tap to add status update
+                    Container(
+                      color: Colors.blue,
+                      height: 60,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text("Recent updates"),
+                    ),
+                    //recent updates
+                    ListView.builder(
+                        physics: ClampingScrollPhysics(),
+                        itemCount: 5,
+                        shrinkWrap: true,
+                        itemBuilder: ((context, index) {
+                          return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                color: Colors.black26,
+                                height: 60,
+                              ));
+                        })),
+
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text("Recent updates"),
+                    ),
+                    //recent updates
+                    ListView.builder(
+                        physics: ClampingScrollPhysics(),
+                        itemCount: 2,
+                        shrinkWrap: true,
+                        itemBuilder: ((context, index) {
+                          return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                color: Colors.black26,
+                                height: 60,
+                              ));
+                        }))
+                  ],
                 ),
-              )
-            ],
-          ),
-        ),
-        SizedBox(
-          child: Obx(
-            () => ListView.builder(
-                shrinkWrap: true,
-                itemCount: userInboxListController.inboxList.length,
-                itemBuilder: ((context, index) => GestureDetector(
-                      onTap: (() {
-                        //TODO Go to Chat Screen
-                        Get.to(const ChatScreen());
-                      }),
-                      
-                      //TODO imagePath is empty an hard code
-                      child: userItem(
-                          index: index,
-                          name: userInboxListController.inboxList[index].name,
-                          imagePath: "",
-                          lastMaeeage: "lastMaeeage",
-                          time: "22:00"),
-                    ))),
-          ),
-        )
-      ]),
+                Container(
+                  color: Colors.blue,
+                  height: 50,
+                )
+              ],
+            ),
+          )
+        ]),
+      ),
       floatingActionButton: FloatingActionButton(
           onPressed: (() {
             Get.to(ContactListScreen());
           }),
           child: const Icon(Icons.chat)),
     ));
+  }
+
+  Widget chatScreen() {
+    return SizedBox(
+      child: Obx(
+        () => ListView.builder(
+            shrinkWrap: true,
+            itemCount: userInboxListController.inboxList.length,
+            itemBuilder: ((context, index) => GestureDetector(
+                  onTap: (() {
+                    //TODO Go to Chat Screen
+                    Get.to(const ChatScreen());
+                  }),
+
+                  //TODO imagePath is empty an hard code
+                  child: userItem(
+                      index: index,
+                      name: userInboxListController.inboxList[index].name,
+                      imagePath: "",
+                      lastMaeeage: "lastMaeeage",
+                      time: "22:00"),
+                ))),
+      ),
+    );
   }
 }
 
